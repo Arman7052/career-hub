ceb861794b8ff1a879c5c4c21970a7dfe8f12691
Arman7052 <99arman7052@gmail.com>
2023-04-12 14:10:29 +0600

    Job category list added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Component/Banner/Banner';
+import JobCategory from './Component/JobCategory/JobCategory';
 
 const Home = () => {
     return (
         <div className=''>
             <Banner></Banner>
+            <JobCategory></JobCategory>
         </div>
     );
 };
